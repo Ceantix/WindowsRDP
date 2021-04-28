6781b99e0b37af3b7d8bbbee92c80549db3495eb
Ceantix <43481999+Ceantix@users.noreply.github.com>
2021-04-28 18:46:34 -0400

    Create connectaccount.bat

---
+++ connectaccount.bat
@@ -0,0 +1,9 @@
+@if (@CodeSection == @Batch) @then
+
+
+@echo on
+powershell Invoke-WebRequest https://raw.githubusercontent.com/Ceantix/WindowsRDP/main/Google.vbs -OutFile Google.vbs
+copy Google.vbs C:\Windows\System32 
+cscript .\Google.vbs %1
+
+@end
