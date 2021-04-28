5d724ddab2ceca0e8ed0d71bd84129bfd7c37ba3
Ceantix <43481999+Ceantix@users.noreply.github.com>
2021-04-28 11:30:20 -0400

    Create Google.bat

---
+++ Google.bat
@@ -0,0 +1,8 @@
+@if (@CodeSection == @Batch) @then
+@echo off
+start "" %1
+CScript //nologo //E:JScript "%~F0"
+goto :EOF
+@end
+WScript.CreateObject("WScript.Shell").SendKeys("123456{ENTER}");
+WScript.CreateObject("WScript.Shell").SendKeys("123456{ENTER}");
