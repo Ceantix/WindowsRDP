4f0ee8f45ef2ceb06aab16e64f3627c27fbe7758
Ceantix <43481999+Ceantix@users.noreply.github.com>
2021-04-28 12:05:00 -0400

    Delete Google.bat

--- Google.bat
+++
@@ -1,8 +0,0 @@
-@if (@CodeSection == @Batch) @then
-@echo on
-echo started
-CScript //nologo //E:JScript "%~F0"
-goto :EOF
-@end
-WScript.CreateObject("WScript.Shell").SendKeys("123456{ENTER}");
-WScript.CreateObject("WScript.Shell").SendKeys("123456{ENTER}");
