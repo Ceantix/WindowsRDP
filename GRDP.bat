dce59aef22e78a9211afc78bf4724033d2944a48
Ceantix <43481999+Ceantix@users.noreply.github.com>
2021-04-28 10:35:29 -0400

    Create GRDP.bat

---
+++ GRDP.bat
@@ -0,0 +1,20 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+curl -O https://raw.githubusercontent.com/Ceantix/WindowsRDP/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/Ceantix/WindowsRDP/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\EVKey Vietnamese Keyboard.exe" https://raw.githubusercontent.com/Ceantix/WindowsRDP/main/EVKey64.exe > out.txt 2>&1
+net user administrator Nagato@123 /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
+echo User: Administrator
+echo Pass: Nagato@123
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
+ping -n 10 127.0.0.1 >nul
