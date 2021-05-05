6916edc21966fa34d37f0e3e9623be33c27643ee
Ceantix <43481999+Ceantix@users.noreply.github.com>
2021-05-05 17:22:34 -0400

    Update connectaccount.bat

--- connectaccount.bat
+++ connectaccount.bat
@@ -2,6 +2,6 @@
 
 
 @echo on
-%1
+"%PROGRAMFILES(X86)%\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="%1" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=%COMPUTERNAME%
 123456
 123456
