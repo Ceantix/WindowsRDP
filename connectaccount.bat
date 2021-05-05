2f18e44882e040d364e8efdb8bd508e6a6e95322
Ceantix <43481999+Ceantix@users.noreply.github.com>
2021-05-05 16:50:13 -0400

    Update connectaccount.bat

--- connectaccount.bat
+++ connectaccount.bat
@@ -2,6 +2,6 @@
 
 
 @echo on
-powershell Invoke-WebRequest https://raw.githubusercontent.com/Ceantix/WindowsRDP/main/Google.vbs -OutFile Google.vbs
-copy Google.vbs C:\Windows\System32 
-cscript .\Google.vbs %1
+%1
+123456
+123456
